9817bba793acd0d57de9a2c4a6e6113f41b90c91
kim <kim@holly.wtf>
2023-06-03 08:03:22 +0300

    constructors ex

---
+++ src/main/java/org/example/Constructor.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Constructor {
+    public Constructor(int num, String string) {
+        System.out.println("Number = " + num + "; Text = " + string);
+    }
+    public Constructor(String string) {
+        System.out.println("Text = " + string);
+    }
+}
